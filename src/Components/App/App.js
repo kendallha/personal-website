a2e506b4cbbe5ae78be44ce2ddb6cc5ab3e5eefb
kendallha <kendallhaw@gmail.com>
2021-06-05 13:19:19 -0700

    Add component folders and install router

---
+++ src/Components/App/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <h1>Hi, I'm Kendall</h1>
+      <img src={`photohere`} />
+      <Nav />
+    </>
+  )
+}
+
+export default App;
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Nav = () => {
+  return(
+    <nav>
+      <Navlink></Navlink>
+    </nav>
+  )
+}
