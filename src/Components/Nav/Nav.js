b5b7aafa1f586b48801e4001b25235ed754bbbae
kendallha <kendallhaw@gmail.com>
2021-06-05 13:22:35 -0700

    Add Nav component

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Navlink } from 'react-router-dom';
 
 const Nav = () => {
   return(
     <nav>
-      <Navlink></Navlink>
+      <Navlink>About</Navlink>
+      <Navlink>Portfolio</Navlink>
+      <Navlink>Resume</Navlink>
+      <Navlink>Blog</Navlink>
+      <Navlink>Contact</Navlink>
     </nav>
   )
 }
